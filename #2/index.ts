b7f37b1736c980a529341087e7c9ceb79ed75a6e
leowhales <leomalayajr@gmail.com>
2022-05-16 20:22:20 -0700

    answers

--- #2/index.ts
+++ #2/index.ts
@@ -4,9 +4,32 @@
  * @param startDateTime 
  * @param endDateTime 
  */
+
+ import dataJson from './data.json';
 export function availability(startDateTime: Date, endDateTime: Date): number {
   // do something
-  return 0;
+  interface data_obj {
+    timestamp: string,
+    status: string,
+  }
+
+  let data: data_obj[];
+  let data_err_count = 0;
+
+  data = dataJson.filter(obj => {
+    let obj_date = new Date(obj.timestamp);
+    return obj_date >= startDateTime &&
+      obj_date < endDateTime;
+  });
+
+  data.forEach(obj => {
+    if (obj.status != "AVAILABLE"){
+      data_err_count += 1;
+    }
+  });
+
+  let res = (data_err_count / data.length) -1;
+  return Math.abs(res);
 }
 
 /**
@@ -18,5 +41,66 @@ export function availability(startDateTime: Date, endDateTime: Date): number {
  */
 export function outages(startDateTime: Date, endDateTime: Date): { type: 'PARTIAL' | 'MAJOR', timestamp: Date, duration: number }[] {
   // do something
-  return [];
+  interface data_obj {
+    timestamp: string,
+    status: string,
+  }
+  
+  interface outage {
+    type: string,
+    timestamp: Date,
+    duration: number,
+  }
+
+  const outages: outage[] = [];
+  let outage_counter = 0;
+  let outage_base_date = '';
+  let outage_type:  "MAJOR" | "PARTIAL" | "" = "";
+  let data: data_obj[];
+  let previous_status = '';
+
+  data = dataJson.filter(obj => {
+    let obj_date = new Date(obj.timestamp);
+    return obj_date >= startDateTime &&
+      obj_date < endDateTime;
+  });
+
+  for (let [idx, obj] of data.entries()) {
+    if (previous_status == '') {
+      previous_status = obj.status;
+    }
+    if (obj.status != 'AVAILABLE'){
+      // init data for recording
+      if (outage_counter == 0) {
+        outage_counter = 1;
+        outage_base_date = obj.timestamp;
+        if (obj.status == 'PARTIALLY_AVAILABLE'){
+          outage_type = 'PARTIAL';
+        }
+        else if (obj.status == 'UNAVAILABLE'){
+          outage_type = 'MAJOR';
+        }
+      }
+      // check if in the middle of counting something
+      if (previous_status == obj.status){
+        // continue adding outage record
+        outage_counter += 1;
+      }
+      // check next data if counting should stop
+      if (data.length -1 == idx || data[idx+1].status != obj.status){
+        // reset and save
+        outages.push({
+          type: outage_type,
+          timestamp: new Date(outage_base_date),
+          duration: outage_counter,
+        });
+        outage_counter = 0;
+        outage_base_date = '';
+        outage_type = '';
+      }
+    }
+    previous_status = obj.status;
+  }
+
+  return outages;
 }
--- #3/index.ts
+++ #3/index.ts
@@ -5,7 +5,51 @@
  * https://www.excelfunctions.net/excel-logest-function.html
  * @param data 
  */
+
+import * as np from 'numjs';
+import * as math from 'mathjs';
+
+function expected_value(x:number[], p:number[]){
+  let expectedValue = 0;
+  for (let a=0; a<x.length; a++){
+      expectedValue += x[a] * (p[a] / x.length);
+  }
+  return expectedValue;
+}
+
+function generate_ones(length:number){
+  let result = [];
+  for (let a=0; a<length; a++){
+      result.push(1);
+  }
+  return result;
+}
+
+function covariance(x: number[], y: number[]){
+  let xy = np.multiply(x,y);
+  let ex = expected_value(x, generate_ones(x.length));
+  let ey = expected_value(y, generate_ones(y.length));
+  let exy = expected_value(xy.tolist(), generate_ones(xy.size));
+  return exy - (ex * ey);
+}
+
 export default function logest(ys: number[]): number {
-  // do something
-  return 0;
+  let y = math.log(ys);
+  let x = [];
+  let res = 0;
+
+  let hasNegative = ys.some(v => v < 0);
+  if (hasNegative) return NaN;
+
+  let hasZero = ys.some(v => v == 0);
+  if (hasZero) return NaN;
+
+  for (let a = 0; a<ys.length; a++){
+    x.push(a);
+  }
+  
+  let covMatrix = (np.array([[covariance(x, x), covariance(x, y)], [covariance(y, x), covariance(y, y)]])).tolist();
+  res = math.exp(covMatrix[0][1] / covMatrix[0][0]);
+
+  return(res);
 }
